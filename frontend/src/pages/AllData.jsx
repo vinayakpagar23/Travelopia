ec2611e0de26e5f1a634da0ffad4f9f5c6215998
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-05-27 12:06:33 +0530

    set data in table format

--- frontend/src/pages/AllData.jsx
+++ frontend/src/pages/AllData.jsx
@@ -3,6 +3,7 @@ import axios from "axios"
 import "../style/alldata.css"
 import { Card, CardBody, CardFooter, CardHeader, Heading, SimpleGrid ,Text,Button} from '@chakra-ui/react';
 import Loading from './Loading';
+import TableFormat from './TableFormat';
 const AllData = () => {
     const[data1,setData] = useState([]);
     const fetchData =async()=>{
@@ -15,20 +16,26 @@ const AllData = () => {
     },[]);
 
   return (
+    <>
     <div className='OuterBox'>
      {data1.length>0?
       data1.map((item)=>(
         <div className='box'>
           <Text><span style={{fontWeight:"bold"}}>Name : </span> {item.name}</Text>
           <Text> <span style={{fontWeight:"bold"}}>email :</span> {item.email}</Text>
+          <Text> <span style={{fontWeight:"bold"}}>email :</span> {item.destination}</Text>
           <Text><span style={{fontWeight:"bold"}}>No_of_treavellers :</span> {item.no_of_travellers}</Text>
           <Text><span style={{fontWeight:"bold"}}>budget_per_Person : $ </span> {item.budgetPerPerson}</Text>
           <Text><span style={{fontWeight:"bold"}}>Total_Budget: $ </span> {item.totalBudget}</Text>
         </div>
       )):<Loading/>
      }
-     
+  
     </div>
+    <TableFormat data={data1}/>
+    </>
+    
+    
   )
 }
 
---
+++ frontend/src/pages/TableFormat.jsx
@@ -0,0 +1,45 @@
+
+import { Table, TableCaption, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
+import React from 'react'
+
+const TableFormat = ({data}) => {
+  return (
+    <> 
+<TableContainer>
+  <Table variant='striped' colorScheme='teal'>
+    <TableCaption>All trip Details</TableCaption>
+    <Thead>
+      <Tr>
+        <Th>Name</Th>
+        <Th>Email</Th>
+        <Th >Destination</Th>
+        <Th isNumeric>No Of travellers</Th>
+        <Th isNumeric>Budget Per Person</Th>
+        <Th isNumeric>Total</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+        {
+            data.map((ele)=>(
+                <Tr>
+                <Td>{ele.name}</Td>
+                <Td>{ele.email}</Td>
+                <Td isNumeric>{ele.destination}</Td>
+                <Td isNumeric>{ele.no_of_trave}</Td>
+                <Td isNumeric>{ele.destination}</Td>
+                <Td isNumeric>{ele.no_of_travellers}</Td>
+                <Td isNumeric>{ele.budgetPerPerson}</Td>
+                <Td isNumeric>{ele.totalBudget}</Td>
+              </Tr>
+            ))
+        }
+      
+    </Tbody>
+  </Table>
+</TableContainer>
+    </>
+  )
+}
+
+export default TableFormat
+
